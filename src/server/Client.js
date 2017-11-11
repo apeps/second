cd078261ac81e1b4e4da3a961e0064cd24cb0504
NatalieTr <radzivilovanp@gmail.com>
2017-11-11 15:11:50 +0200

    [New] Basic WebSocket server API implementation

--- src/server/Client.js
+++ src/server/Client.js
@@ -11,26 +11,61 @@ const generateClientId = () => crypto.randomBytes(16).toString("hex");
 
 export default class Client {
 
-	id = generateClientId();
-	radio = new Radio();
+    id = generateClientId();
+    name = "Anonymous";
+    /**
+     * @type {Radio}
+     */
+    radio = new Radio();
 
-	constructor (ws, props = {}) {
+    constructor(ws, props = {}) {
 
-		this.radio.renew(ws);
+        this.radio.renew(ws);
 
-		// bind radio callbacks here
+        this.init();
 
-		clients.set(this.id, this);
-		console.log(`New client from IP=${ props.ip }, client ID=${ this.id }`);
+        clients.set(this.id, this);
+        console.log(`New client from IP=${ props.ip }, client ID=${ this.id }`);
 
-	}
+    }
 
-	onExit () {
+    onExit() {
 
-		console.log(`Client ID=${ this.id } is gone.`);
-		clients.delete(this.id);
-		this.radio.end();
+        console.log(`Client ID=${ this.id } is gone.`);
+        clients.delete(this.id);
+        this.radio.end();
 
-	}
+    }
+
+    init() {
+
+        this.radio.listen("message", (msg) => {
+
+            if (!msg || typeof(msg.text) === "string") {
+                console.log("Error! The message is not valid.");
+                return;
+            }
+            for (const client of clients.values()) {
+                console.log(client);
+                this.radio.tell("message", {
+                    text: msg.text,
+                    name: client.name,
+                    id: client.id
+                });
+            }
+
+        });
+
+        this.radio.listen("config", (conf) => {
+
+            if (!conf || typeof(conf.name) === "string") {
+                console.log("Error! The configuration is not valid.");
+                return;
+            }
+            this.name = conf.name;
+
+        });
+
+    }
 
 }
